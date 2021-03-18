326919cacb7d37dfd7355cd5e1613bd0b3c08325
xeada77 <luismartq@gmail.com>
2021-03-18 17:34:40 +0100

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGODB:
-    "mongodb+srv://appdevpc:IoqbgpUyWrzRZ39s@miclusterdepruebas.fkukt.mongodb.net/merng?retryWrites=true&w=majority",
-  SECRET_KEY: "supercalifragilisticoespialidoso",
-};
